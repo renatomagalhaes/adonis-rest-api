e2ea7451db9a5edaf6b9a5db4867ccbcdccce06f
Renato Magalhães <rsmdbmaster@gmail.com>
2021-05-08 23:04:39 -0300

    create and reg acl middleware

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,24 +1,17 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
-  public async index ({}: HttpContextContract) {
+  public async store ({ request, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+
+    const token = await auth.attempt(email, password, {
+      expiresIn: '30 days'
+    })
+
+    return token
   }
 
-  public async create ({}: HttpContextContract) {
-  }
-
-  public async store ({}: HttpContextContract) {
-  }
-
-  public async show ({}: HttpContextContract) {
-  }
-
-  public async edit ({}: HttpContextContract) {
-  }
-
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
+  public async destroy ({ auth }: HttpContextContract) {
+    await auth.logout()
   }
 }
